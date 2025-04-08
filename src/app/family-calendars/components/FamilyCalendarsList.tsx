5fd0d825a3ccbef1e4b5e1692447f78c144a1e53
Wojciech Białek <w.bialek@merixstudio.com>
2025-04-08 13:06:17 +0200

    husky test5

--- src/app/family-calendars/components/FamilyCalendarsList.tsx
+++ src/app/family-calendars/components/FamilyCalendarsList.tsx
@@ -1,44 +1,42 @@
-"use client";
-import { usePaginatedQuery } from "@blitzjs/rpc";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import getFamilyCalendars from "../queries/getFamilyCalendars";
-import { useSearchParams } from "next/navigation";
-import { usePathname } from "next/navigation";
-import { Route } from "next";
+"use client"
+import { usePaginatedQuery } from "@blitzjs/rpc"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import getFamilyCalendars from "../queries/getFamilyCalendars"
+import { useSearchParams } from "next/navigation"
+import { usePathname } from "next/navigation"
+import { Route } from "next"
 
-const ITEMS_PER_PAGE = 100;
+const ITEMS_PER_PAGE = 100
 
 export const FamilyCalendarsList = () => {
-  const searchparams = useSearchParams()!;
-  const page = Number(searchparams.get("page")) || 0;
+  const searchparams = useSearchParams()!
+  const page = Number(searchparams.get("page")) || 0
   const [{ familyCalendars, hasMore }] = usePaginatedQuery(getFamilyCalendars, {
     orderBy: { id: "asc" },
     skip: ITEMS_PER_PAGE * page,
     take: ITEMS_PER_PAGE,
-  });
-  const router = useRouter();
-  const pathname = usePathname();
+  })
+  const router = useRouter()
+  const pathname = usePathname()
 
   const goToPreviousPage = () => {
-    const params = new URLSearchParams(searchparams);
-    params.set("page", (page - 1).toString());
-    router.push((pathname + "?" + params.toString()) as Route);
-  };
+    const params = new URLSearchParams(searchparams)
+    params.set("page", (page - 1).toString())
+    router.push((pathname + "?" + params.toString()) as Route)
+  }
   const goToNextPage = () => {
-    const params = new URLSearchParams(searchparams);
-    params.set("page", (page + 1).toString());
-    router.push((pathname + "?" + params.toString()) as Route);
-  };
+    const params = new URLSearchParams(searchparams)
+    params.set("page", (page + 1).toString())
+    router.push((pathname + "?" + params.toString()) as Route)
+  }
 
   return (
     <div>
       <ul>
         {familyCalendars.map((familyCalendar) => (
           <li key={familyCalendar.id}>
-            <Link href={`/family-calendars/${familyCalendar.id}`}>
-              {familyCalendar.name}
-            </Link>
+            <Link href={`/family-calendars/${familyCalendar.id}`}>{familyCalendar.name}</Link>
           </li>
         ))}
       </ul>
@@ -50,5 +48,5 @@ export const FamilyCalendarsList = () => {
         Next
       </button>
     </div>
-  );
-};
+  )
+}
