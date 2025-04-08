97d19e77fa56e8893b12dc4a57684ae8f111bac3
Wojciech Białek <w.bialek@merixstudio.com>
2025-04-08 13:32:15 +0200

    husky test8

--- src/app/family-calendars/mutations/updateFamilyCalendar.ts
+++ src/app/family-calendars/mutations/updateFamilyCalendar.ts
@@ -1,17 +1,26 @@
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { UpdateFamilyCalendarSchema } from "../schemas";
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { UpdateFamilyCalendarSchema } from "../schemas"
 
 export default resolver.pipe(
   resolver.zod(UpdateFamilyCalendarSchema),
   resolver.authorize(),
-  async ({ id, ...data }) => {
+  async ({ id, ...data }, ctx) => {
+    const userId = ctx.session.userId
+
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const familyCalendar = await db.familyCalendar.update({
       where: { id },
-      data,
-    });
+      data: {
+        ...data,
+        superAdmin: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    })
 
-    return familyCalendar;
+    return familyCalendar
   }
-);
+)
